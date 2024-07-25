24750a4d1158945c3b9f554fd122e91589357d0f
Jonas Lindau <jonas@jonika.nu>
2024-07-25 10:56:35 +0200

    added model and data for action button

--- model/hero.py
+++ model/hero.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 
 @dataclass()
+class ActionButton:
+    caption: str
+    targeturl: str
+    
+@dataclass()
 class Hero:
     heading: str
     intro: str
     image: str
     imagealt: str
+    actionbutton: ActionButton
