09004b3bf1861bf0474f49ae415dd448bc3d034d
Jonas Lindau <jonas@jonika.nu>
2024-07-26 11:24:35 +0200

    Added data, model and mark-up for about page

--- model/aboutpage.py
+++ model/aboutpage.py
@@ -1,14 +1,22 @@
 from dataclasses import dataclass, field
 from typing import Optional
 from model.sitesettings import SiteSettings
+from model.story import Story
 import json
 
 @dataclass()
+class AboutStory(Story):
+    image: str
+    imagealt: str
+
+@dataclass()
 class AboutPage:
+    heading: str
+    intro: str
+    stories: list[AboutStory]
     sitesettings: Optional[SiteSettings] = field(default_factory=lambda: None)
         
 def get_aboutpage():
-    return AboutPage()
-    # with open('./content/startpage.json') as file:
-    #     data = json.load(file)
-    #     return Startpage(**data)
+    with open('./content/about.json') as file:
+        data = json.load(file)
+        return AboutPage(**data)
